f4e8594faba10947c5789a988679d8cfaebf11f9
thiago.paiva <thiago.paiva@institutoiracema.org.br>
2023-09-08 08:13:37 -0300

    gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
-#include <Arduino.h>
-#include <HardwareSerial.h>
-#include <stdio.h>
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Arduino.h>
+#include <HardwareSerial.h>
 #include <SPI.h>
+#include <stdio.h>
 
 #include "driver/gpio.h"
 #include "driver/uart.h"
 #include "esp_log.h"
+#include "fonte.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
-#include "fonte.h"
 
 #define XInicial 0
 #define Yinicial 63
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixelss
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixelss
 #define LETRA_POR_DISPLAY 5
 #define CELA_WIDTH 25
 
@@ -28,28 +26,51 @@
 #define OLED_CS1 11
 #define OLED_RESET 5
 
-extern "C"
-{
-    void app_main(void);
+extern "C" {
+void app_main(void);
 }
 
-void app_main()
-{
-    Adafruit_SSD1306 _display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS1);
-    _display.begin(SSD1306_SWITCHCAPVCC);
-    _display.clearDisplay();                 // limpa o display
-    _display.setFont(&BrailleNeue33pt8b);    // seta a fonte utilizada
-    _display.setTextSize(1);                 // Normal 1:1 pixel scale
-    _display.setTextColor(SSD1306_INVERSE);  // Draw white text
-    _display.setCursor(XInicial, Yinicial);  // Start at top-left corner
-    _display.cp437(true);                    // Use full 256 char 'Code Page 437' font
-    _display.write("\0");
-    _display.display();
-    _display.write('o');
-    _display.write('i');
+void app_main() {
+  gpio_set_direction(GPIO_NUM_45, GPIO_MODE_OUTPUT);
 
-    while (1)
-    {
-        
-    }
+  uart_config_t uart_config = {
+      .baud_rate = 115200,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+  };
+
+  uart_param_config(UART_NUM_0, &uart_config);
+  uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+  uart_driver_install(UART_NUM_0, 256, 0, 0, NULL, 0);
+
+  while (!uart_is_driver_installed(UART_NUM_0)) {
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+
+  println("init");
+
+  Adafruit_SSD1306 _display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS1);
+
+  while ((!_display[i].begin(SSD1306_SWITCHCAPVCC))) {
+    println("um erro occorreu no SSD1306: ");
+    delay(50);
+  }
+
+  _display.begin(SSD1306_SWITCHCAPVCC);
+  _display.clearDisplay();                 // limpa o display
+  _display.setFont(&BrailleNeue33pt8b);    // seta a fonte utilizada
+  _display.setTextSize(1);                 // Normal 1:1 pixel scale
+  _display.setTextColor(SSD1306_INVERSE);  // Draw white text
+  _display.setCursor(XInicial, Yinicial);  // Start at top-left corner
+  _display.cp437(true);                    // Use full 256 char 'Code Page 437' font
+  _display.write("\0");
+  _display.display();
+  _display.write('o');
+  _display.write('i');
+
+  while (1) {
+  }
 }
